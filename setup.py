11aa4f46b1449eaea833fd0e8615cff3ee698f51
si <pettibone@gmail.com>
2017-12-05 18:48:19 +0700

    installable as a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shapeshiftio',
+        version='0.1.1',
+        description='Python API for https://shapeshift.io',
+        url='http://github.com/skeebuzz/pyShapeshift',
+        author='skeebuzz',
+        author_email='pettibone@gmail.com',
+        license='MIT',
+        packages=['pyshapeshift'],
+        zip_safe=False)
